dc987dcc81db2bfea2b3f2960fabef0d1275f5b3
CostasLucas <lucas.costas@ciclosmontecastelo.com>
2024-12-18 20:18:17 +0100

    Primer commit - Configuración inicial del proyecto

---
+++ app/src/main/java/com/lucas/voluntariado/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lucas.voluntariado
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lucas.voluntariado.ui.theme.GestorVoluntariadoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GestorVoluntariadoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GestorVoluntariadoTheme {
+        Greeting("Android")
+    }
+}
